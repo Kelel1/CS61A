07a98c9f904ce82c585d700d473db02f3b286949
Kern Elder <kelel1@yahoo.com>
2018-08-12 17:07:16 -0400

    Complete hailstone function

--- hw01/hw01.py
+++ hw01/hw01.py
@@ -103,3 +103,15 @@ def hailstone(n):
     7
     """
     "*** YOUR CODE HERE ***"
+    print(n)
+    count = 1
+    while (n != 1):
+        if (n % 2 == 0):
+            n = n / 2
+            count+=1           
+            print(int(n))
+        else:     
+            n = (n * 3) + 1
+            count+=1                      
+            print(int(n))
+    return count
